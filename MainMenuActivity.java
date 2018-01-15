811f68260d7c96ebe18fbb0778ef6a6497bfc633
Brian <brian@1108software.com>
2018-01-15 17:59:49 +0000

    small change

--- MainMenuActivity.java
+++ MainMenuActivity.java
@@ -1,13 +1,10 @@
 package com.a1108software.brian.fly_safe;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,13 +12,8 @@ import android.widget.GridView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -44,7 +36,7 @@ public class MainMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu1);
+        setContentView(R.layout.activity_main_menu);
 
         // Initialize Firebase Auth
         mFirebaseAuth = FirebaseAuth.getInstance();
